a6d8284f19dda0842aca3f3af79255366741dd7c
Annie Huang <annie.hn@gmail.com>
2020-12-09 21:49:11 +1100

    Introduce local variable people through useState

--- 1-birthday-reminder/src/App.js
+++ 1-birthday-reminder/src/App.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
+
+// Because in data.js, it has:
+// export default [..]
+// You can name it whatever name you want (the first word data), don't have to be 'data'
 import data from './data';
+
 import List from './List';
+
 function App() {
+  const [people, setPeople] = useState(data);
   return <main>
     <section className="container">
-      <h3>0 birthdays today</h3>
-      <List />
+      <h3>{people.length} birthdays today</h3>
+      <List people={people} />
       <button onClick={() => console.log('you clicked me')}>clear all</button>
     </section>
   </main>;
