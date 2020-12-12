35f73ce09ceaadc3c04509a53a6bc1203dd1495b
Annie Huang <annie.hn@gmail.com>
2020-12-12 15:36:53 +1100

    Allow refresh page when all cards are deleted. Very good explaination on the different of  onClick={() => fetchTours()}  vs onClick={fetchTours} vs onClick={fetchTours()}

--- 2-tours/src/App.js
+++ 2-tours/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
+import Tour from "./Tour";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
@@ -34,6 +35,16 @@ function App() {
 
   if (loading) {
     return <main><Loading /></main>
+  }
+
+  if(tours.length === 0) {
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        {/*<button className='btn' onClick={() => fetchTours()}>Refresh</button>*/}
+        <button className='btn' onClick={fetchTours}>Refresh</button>
+      </div>
+    </main>
   }
 
   return <main>
