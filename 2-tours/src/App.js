a3196ba53e9a545f752799648d950cfb791a4b1b
Annie Huang <annie.hn@gmail.com>
2020-12-12 14:27:35 +1100

    Fetch the tours info from api on page load.

--- 2-tours/src/App.js
+++ 2-tours/src/App.js
@@ -6,13 +6,30 @@ import Tours from './Tours'
 const url = 'https://course-api.com/react-tours-project'
 
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [tour, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      console.log(tours);
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
   if (loading) {
     return <main><Loading /></main>
   }
-
 
   return <main>
     <Tours />
