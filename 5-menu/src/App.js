2f63fc21f5f36d73bd0d5337bd2d01e597d09c0d
Annie Huang <annie.hn@gmail.com>
2020-12-13 12:55:04 +1100

    Create the template content of App.js

--- 5-menu/src/App.js
+++ 5-menu/src/App.js
@@ -17,7 +17,18 @@ function App() {
 
   console.log('categories=', categories);
 
-  return <h2>menu project setup</h2>;
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
