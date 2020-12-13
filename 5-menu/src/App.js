bd7c1a35ba396a6c5ff5ccf7dc2bcbf16feb8128
Annie Huang <annie.hn@gmail.com>
2020-12-13 12:39:48 +1100

    My trial of getting the category list.

--- 5-menu/src/App.js
+++ 5-menu/src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    // Both method work:
+    // const newCategories = menuItems.map(item => item.category)
+    //   .filter((value, index, self) => self.indexOf(value) === index)
+    const newCategories = [...new Set( menuItems.map(item => item.category))];
+    setCategories(newCategories);
+  }, [])
+
+  console.log('categories=', categories);
+
   return <h2>menu project setup</h2>;
 }
 
