49cbca1456c9131ae64232baec36baa6ad7631ef
Annie Huang <annie.hn@gmail.com>
2020-12-13 15:42:03 +1100

    Instead of getting all the unique category keywords in useEffect, do it outside the App function and inject it straight away into categories through useState call.

--- 5-menu/src/App.js
+++ 5-menu/src/App.js
@@ -3,18 +3,25 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// const allCategories = items
+//   .map(item => item.category)
+//   .filter((value, index, self) => self.indexOf(value) === index)
+const allCategories = ['all', ...new Set(items.map(item => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
 
-  // I thought we will have to come up with the categories names ourselves.
-  useEffect(() => {
-    // Both method work:
-    // const newCategories = menuItems.map(item => item.category)
-    //   .filter((value, index, self) => self.indexOf(value) === index)
-    const newCategories = [...new Set( menuItems.map(item => item.category))];
-    setCategories(newCategories);
-  }, [])
+  console.log('allCategories=', allCategories);
+
+  // // I thought we will have to come up with the categories names ourselves.
+  // useEffect(() => {
+  //   // Both method work:
+  //   // const newCategories = menuItems.map(item => item.category)
+  //   //   .filter((value, index, self) => self.indexOf(value) === index)
+  //   const newCategories = [...new Set( menuItems.map(item => item.category))];
+  //   setCategories(newCategories);
+  // }, [])
 
   const filterItems = (category) => {
     if (category === 'all') {
