ba7e055d7ef7931dad991f01dbf3c40173b49229
Annie Huang <annie.hn@gmail.com>
2020-12-16 14:27:34 +1100

    Remove the Alert after 3 seconds through useEffect. Very important: If you use want to update the children after the timeout, the useEffect need to set on the children

--- 10-grocery-bud/src/Alert.js
+++ 10-grocery-bud/src/Alert.js
@@ -1,5 +1,14 @@
 import React, { useEffect } from 'react';
 
-const Alert = ({ type, msg }) => <p className={`alert alert-${type}`}>{msg}</p>;
+const Alert = ({ type, msg, removeAlert }) => {
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, []); // Whenever input property change, the whole component will re-render so you can keep [] in useEffect
+
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+};
 
 export default Alert;
