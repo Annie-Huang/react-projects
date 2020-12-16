9f8a8077c83dc5f3be940bf01c01af5a9d7c4b5e
Annie Huang <annie.hn@gmail.com>
2020-12-16 14:39:38 +1100

    Implement the function to clear the whole list.

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -36,6 +36,11 @@ function App() {
     setAlert({ show, msg, type });
   };
 
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list');
+    setList([]);
+  };
+
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
@@ -57,7 +62,9 @@ function App() {
       {list.length > 0 && (
         <div className='grocery-container'>
           <List items={list} />
-          <button className='clear-btn'>clear items</button>
+          <button className='clear-btn' onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
