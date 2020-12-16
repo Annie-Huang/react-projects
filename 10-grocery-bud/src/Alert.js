92aa021765799c037b111728c1e8705f04f23091
Annie Huang <annie.hn@gmail.com>
2020-12-16 15:14:15 +1100

    Important!!! Want to reset the timeout to 3s every time a list change. So it can always delay for 3 seconds (for the last alert) whenever we action multiple alert within the 3s.

--- 10-grocery-bud/src/Alert.js
+++ 10-grocery-bud/src/Alert.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 
-const Alert = ({ type, msg, removeAlert }) => {
+const Alert = ({ type, msg, removeAlert, list }) => {
   useEffect(() => {
     let timeout = setTimeout(() => {
       removeAlert();
     }, 3000);
     return () => clearTimeout(timeout);
-  }, []); // Whenever input property change, the whole component will re-render so you can keep [] in useEffect
+
+    // watch against the list change, otherwise the 3 seconds only apply on the first 3 seconds interval call.
+    // e.g if user add an item, wait for 2s and then clear all items, the 2nd alert msg will only appear for 1s, rather than resetting the 3s clock again.
+  }, [list]);
 
   return <p className={`alert alert-${type}`}>{msg}</p>;
 };
