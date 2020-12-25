391c3c8285b06208878c05efe8e608cc5ad4fe58
Annie Huang <annie.hn@gmail.com>
2020-12-25 17:41:57 +1100

    Add the close button, links and menu links and social icons for the <Sidebar>

--- 12-sidebar-modal/src/Sidebar.js
+++ 12-sidebar-modal/src/Sidebar.js
@@ -8,7 +8,33 @@ const Sidebar = () => {
     <aside className={`sidebar show-sidebar`}>
       <div className='sidebar-header'>
         <img src={logo} className='logo' alt='coding addict' />
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
       </div>
+      <ul className='links'>
+        {links.map(link => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className='social-icons'>
+        {social.map(link => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
     </aside>
   );
 };
