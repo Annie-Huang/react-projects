030cb521a6d94b6620df390b93dc039c8800c0b0
Annie Huang <annie.hn@gmail.com>
2020-12-25 18:46:11 +1100

    Instead of passing one value, passing an object that contains variables and functions to AppContext.Provider.

--- 12-sidebar-modal/src/context.js
+++ 12-sidebar-modal/src/context.js
@@ -3,9 +3,39 @@ import React, { useState, useContext } from 'react';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 // custom hook
+// Your custom hook must start with useXXX, otherwise it will not compiled.
+// You can only use the react hook in a component or in a custom hook.
 export const useGlobalContext = () => useContext(AppContext);
 
 export { AppContext, AppProvider };
