69137ae9ba07c25bfb6b1fddfbc356b6143ac0bb
Annie Huang <annie.hn@gmail.com>
2020-12-25 18:55:09 +1100

    const { isModalOpen, closeModal } = useGlobalContext(); in Modal.js and use it in the template

--- 12-sidebar-modal/src/Modal.js
+++ 12-sidebar-modal/src/Modal.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+
 const Modal = () => {
+  const { isModalOpen, closeModal } = useGlobalContext();
+
   //  show-modal was the class to control whether we see it or not.
   return (
     // <div className={`modal-overlay show-modal`}>
-    <div className={`modal-overlay`}>
+    <div className={`modal-overlay ${isModalOpen ? 'show-modal' : ''}`}>
       <div className='modal-container'>
         <h3>modal content</h3>
-        <button className='close-modal-btn'>
+        <button className='close-modal-btn' onClick={closeModal}>
           <FaTimes />
         </button>
       </div>
