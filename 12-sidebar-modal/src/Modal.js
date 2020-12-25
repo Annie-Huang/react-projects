767e88bc34c18d1ed8ee067a6151ac6ef69c759c
Annie Huang <annie.hn@gmail.com>
2020-12-25 17:00:53 +1100

    Reformat <Home> and <Modal> so "jsxSingleQuote" can take effect

--- 12-sidebar-modal/src/Modal.js
+++ 12-sidebar-modal/src/Modal.js
@@ -5,9 +5,9 @@ const Modal = () => {
   return (
     // <div className={`modal-overlay show-modal`}>
     <div className={`modal-overlay`}>
-      <div className="modal-container">
+      <div className='modal-container'>
         <h3>modal content</h3>
-        <button className="close-modal-btn">
+        <button className='close-modal-btn'>
           <FaTimes />
         </button>
       </div>
