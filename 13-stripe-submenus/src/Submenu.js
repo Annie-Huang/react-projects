b5ae62b5d90d07c6f082f3e28a9e2baa8abd54ec
Annie Huang <annie.hn@gmail.com>
2020-12-26 22:46:41 +1100

    Get location from useGlobalContext in Submenu.js and use useRef and useEffect to manually position the submenu container.

--- 13-stripe-submenus/src/Submenu.js
+++ 13-stripe-submenus/src/Submenu.js
@@ -2,10 +2,20 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useGlobalContext } from './context';
 
 const Submenu = () => {
-  const { isSubmenuOpen } = useGlobalContext();
+  const { isSubmenuOpen, location } = useGlobalContext();
+  const container = useRef(null);
+
+  useEffect(() => {
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+  }, [location]);
 
   return (
-    <aside className={`submenu ${isSubmenuOpen ? 'show' : ''}`}>submenu</aside>
+    <aside className={`submenu ${isSubmenuOpen ? 'show' : ''}`} ref={container}>
+      submenu
+    </aside>
   );
 };
 
