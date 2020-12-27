9c62a41cf75f95ecab79a34b10d0fd281c80190e
Annie Huang <annie.hn@gmail.com>
2020-12-27 20:40:18 +1100

    Do the same for remove button: Add method to remove (to dispatch action)  and assign to AppContext in context.js. Add logic in reducer.js. get 'remove' from useGlobalContext and call it in the template in CartItem.js

--- 14-cart/src/reducer.js
+++ 14-cart/src/reducer.js
@@ -7,6 +7,12 @@ const reducer = (state, action) => {
       cart: [],
     };
   }
+  if (action.type === 'REMOVE') {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  }
   return state;
 };
 
