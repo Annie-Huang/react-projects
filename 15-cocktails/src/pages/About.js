df0b7e9d9ee355e3cb02029c8d4050f971a37a15
Annie Huang <annie.hn@gmail.com>
2020-12-28 13:24:09 +1100

    Construct About and Error page.

--- 15-cocktails/src/pages/About.js
+++ 15-cocktails/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus nam
+        quibusdam quidem soluta vel! Animi blanditiis corporis doloribus dolorum
+        eaque, eius, in iusto, molestiae mollitia quam quibusdam tempora? Cum
+        deleniti in officiis totam ut. Enim eveniet natus quisquam repellat
+        tenetur.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
