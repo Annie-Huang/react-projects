75123945d53f6b23b97c791f2fae76e88bd0f450
Annie Huang <annie.hn@gmail.com>
2020-12-28 22:38:47 +1100

    e.preventDefault(); to prevent action when user click 'Enter' key.

--- 15-cocktails/src/components/SearchForm.js
+++ 15-cocktails/src/components/SearchForm.js
@@ -4,19 +4,21 @@ import { useGlobalContext } from '../context';
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
   const searchValue = React.useRef('');
-  const searchCocktail = () => {
-    // console.log(searchValue);
-    setSearchTerm(searchValue.current.value);
-  };
-
   // set focus on input on pageload (useEffect, useRef)
   React.useEffect(() => {
     searchValue.current.focus();
   }, []);
 
+  const searchCocktail = () => {
+    // console.log(searchValue);
+    setSearchTerm(searchValue.current.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <section className='section search'>
-      <form className='search-form'>
+      <form className='search-form' onSubmit={handleSubmit}>
         <div className='form-control'>
           <label htmlFor='name'>search your favorite cocktail</label>
           <input
