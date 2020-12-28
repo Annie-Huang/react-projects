46182854fb30caec3894eb9ed06a19943e8879dd
Annie Huang <annie.hn@gmail.com>
2020-12-29 09:12:11 +1100

    Destruct the return data into a cocktail object.

--- 15-cocktails/src/pages/SingleCocktail.js
+++ 15-cocktails/src/pages/SingleCocktail.js
@@ -20,7 +20,36 @@ const SingleCocktail = () => {
         const response = await fetch(`${url}${id}`);
         const data = await response.json();
         if (data.drinks) {
-          setCocktail(data.drinks[0]);
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0];
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+          setCocktail(newCocktail);
         } else {
           setCocktail(null);
         }
